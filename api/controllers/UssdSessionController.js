7da099833c9588b553e43bc199af9d0514838300
Dulitha Ruvin Daluwatta <dulitha.ruvin@gmail.com>
2017-09-30 16:41:39 +0530

    ussdsession code api added

---
+++ api/controllers/UssdSessionController.js
@@ -0,0 +1,11 @@
+/**
+ * UssdSessionController
+ *
+ * @description :: Server-side logic for managing ussdsessions
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
