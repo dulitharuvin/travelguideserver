a4c01011fa177dc911a37f7c2cec68e4a5b648de
Dulitha Ruvin Daluwatta <dulitha.ruvin@gmail.com>
2017-09-30 23:35:14 +0530

    Environment updated 3.0

--- config/env/development.js
+++ config/env/development.js
@@ -18,7 +18,7 @@ module.exports = {
    ***************************************************************************/
 
   models: {
-    connection: 'mongodbDevelopment'
+    connection: 'mongodbProduction'
   }
 
 };
